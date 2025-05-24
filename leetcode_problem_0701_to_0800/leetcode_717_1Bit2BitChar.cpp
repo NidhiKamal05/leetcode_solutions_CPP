f827b4f5b85a6656c6b2533a66e0744ab610d967
Preeti Kamal <pritikamal006@gmail.com>
2025-05-24 17:17:01 +0530

    Day-45

--- leetcode_problem_0701_to_0800/leetcode_717_1Bit2BitChar.cpp
+++ leetcode_problem_0701_to_0800/leetcode_717_1Bit2BitChar.cpp
@@ -6,19 +6,6 @@
 
 using namespace std ;
 
-/** JAVA SOLUTION **/
-/*class Solution {
-    public boolean isOneBitCharacter(int[] bits) {
-        int n = bits.length ;
-        for(int i=0; i<n-1; ++i) {
-            if((bits[i] == 1) && (i++ == n-2)) {
-                return false ;
-            }
-        }
-        return true ;
-    }
-}*/
-
 bool isOneBitCharcater(vector<int>& bits) {
 	int n = bits.size() ;
     for(int i=0; i<n-1; ++i) {
