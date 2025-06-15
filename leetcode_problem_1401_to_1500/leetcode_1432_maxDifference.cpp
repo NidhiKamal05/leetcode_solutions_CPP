ec9314b08dd3f79032bef1cce9500bc644a6a091
Preeti Kamal <pritikamal006@gmail.com>
2025-06-15 22:40:17 +0530

    Day-64

--- leetcode_problem_1401_to_1500/leetcode_1432_maxDifference.cpp
+++ leetcode_problem_1401_to_1500/leetcode_1432_maxDifference.cpp
@@ -6,33 +6,6 @@
 
 using namespace std ;
 
-/** JAVA SOLUTION **/
-/*class Solution {
-    public int maxDiff(int num) {
-        String s1 = String.valueOf(num) ;
-        String s2 = s1 ;
-        for(char ch : s1.toCharArray()) {
-            if(ch != '9') {
-                s1 = s1.replace(ch, '9') ;
-                break ;
-            }
-        }
-        char ch = s2.charAt(0) ;
-        char c = '1' ;
-        if(ch == '1') {
-            for(int i=1; i<s2.length(); ++i) {
-                if(s2.charAt(i) != '0' && s2.charAt(i) != ch) {
-                    ch = s2.charAt(i) ;
-                    c = '0' ;
-                    break ;
-                }
-            }
-        }
-        s2 = s2.replace(ch, c) ;
-        return Integer.valueOf(s1) - Integer.valueOf(s2) ;
-    }
-}*/
-
 int maxDiff(int num) {
     string s1 = to_string(num) ;
     string s2 = s1 ;
