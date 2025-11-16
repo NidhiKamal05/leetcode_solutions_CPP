6f3e3b1e6214fca5611ef97b7c88207497bbf397
Preeti Kamal <pritikamal006@gmail.com>
2025-11-16 17:56:39 +0530

    Day-173

---
+++ leetcode_problem_1501_to_1600/leetcode_1513_numberOfSubstringsWithOnly1s.cpp
@@ -0,0 +1,67 @@
+
+// 1513. NUMBER OF SUBSTRINGS WITH ONLY 1S
+
+#include<iostream>
+
+using namespace std ;
+
+/** JAVA SOLUTION **/
+/*class Solution {
+    public int numSub(String s) {
+        final int MOD = 1000000007 ;
+        long ans = 0 ;
+        for(int i=0; i<s.length(); ++i) {
+            if(s.charAt(i) == '1') {
+                long n = 0 ;
+                while(i < s.length() && s.charAt(i) != '0') {
+                    ++n ;
+                    ++i ;
+                }
+                --i ;
+                ans += n * (n + 1) / 2 ;
+            }
+        }
+        return (int) (ans % MOD) ;
+    }
+}*/
+
+int numSub(string s) {
+    const int MOD = 1000000007 ;
+    long long ans = 0 ;
+    for(int i=0; i<s.length(); ++i) {
+        if(s[i] == '1') {
+            long long n = 0 ;
+            while(i < s.length() && s[i] != '0') {
+                ++n ;
+                ++i ;
+            }
+            --i ;
+            ans += n * (n + 1) / 2 ;
+        }
+    }
+    return ans % MOD ;
+}
+
+int main() {
+	int ans ;
+	string s ;
+	
+	cout << endl ;
+	cout << "  NUMBER OF SUBSTRINGS WITH ONLY 1S " << endl ;
+	cout << " -----------------------------------" << endl ;
+	
+	cout << endl ;
+	cout << "Enter a binary string, s = " ;
+	cin >> s ;
+	
+	ans = numSub(s) ;
+	
+	cout << endl ;
+	cout << "Number of substrings = " << ans << endl ;
+	
+	cout << endl ;
+	
+	system("pause") ;
+	
+	return 0 ;
+}
