b3d5537173018b529de33c263d3f131bd1287f36
Preeti Kamal <pritikamal006@gmail.com>
2025-12-02 19:13:38 +0530

    Day-186

--- leetcode_problem_0001_to_0100/leetcode_98_validateBST.cpp
+++ leetcode_problem_0001_to_0100/leetcode_98_validateBST.cpp
@@ -1,44 +1,6 @@
 
 // 98. VALIDATE BINARY SEARCH TREE
 
-/** JAVA SOLUTION **/
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-/*class Solution {
-    public List<Integer> l = new ArrayList<>() ;
-    private void inOrder(TreeNode root) {
-        if(root != null) {
-            inOrder(root.left) ;
-            l.add(root.val) ;
-            inOrder(root.right) ;
-        }
-    }
-    public boolean isValidBST(TreeNode root) {
-        inOrder(root) ;
-        for(int i=0; i<l.size()-1; ++i) {
-            if(l.get(i) >= l.get(i+1)) {
-                return false ;
-            }
-        }
-        return true ;
-    }
-}*/
-
-/** C++ SOLUTION **/
-
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
