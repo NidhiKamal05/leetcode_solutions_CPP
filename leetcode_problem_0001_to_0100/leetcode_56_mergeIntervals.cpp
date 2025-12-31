800cb6ee6571ce9dbb7f103ee7a0ddb00ba52656
Nidhi Kamal <nidhikamal05@gmail.com>
2025-12-31 17:42:58 +0530

    Day-200

--- leetcode_problem_0001_to_0100/leetcode_56_mergeIntervals.cpp
+++ leetcode_problem_0001_to_0100/leetcode_56_mergeIntervals.cpp
@@ -7,31 +7,6 @@
 
 using namespace std ;
 
-/** JAVA SOLUTION **/
-/*class Solution {
-    public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0],b[0])) ;
-        int n = intervals.length ;
-        List<int[]> list = new ArrayList<>() ;
-        list.add(intervals[0]) ;
-        for(int i=1; i<n; ++i) {
-            int back = list.size()-1 ;
-            int[] l = list.get(back) ;
-            if(l[1] >= intervals[i][0]) {
-                l[1] = Math.max(l[1], intervals[i][1]) ;
-            }
-            else {
-                list.add(intervals[i]) ;
-            }
-        }
-        int[][] result = new int[list.size()][2] ;
-        for(int i=0; i<list.size(); ++i) {
-            result[i] = list.get(i) ;
-        }
-        return result ;
-    }
-}*/
-
 vector<vector<int>> merge(vector<vector<int>>& intervals) {
     sort(intervals.begin(), intervals.end()) ;
     vector<vector<int>> result ;
