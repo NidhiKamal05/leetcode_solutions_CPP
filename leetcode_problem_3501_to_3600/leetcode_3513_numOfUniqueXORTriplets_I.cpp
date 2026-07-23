32458b73fc184d95ea95ba99dfc5cf431265b75d
Preeti Kamal <pritikamal006@gmail.com>
2026-07-23 19:32:31 +0530

    Day-359

---
+++ leetcode_problem_3501_to_3600/leetcode_3513_numOfUniqueXORTriplets_I.cpp
@@ -0,0 +1,56 @@
+
+// 3513. NUMBER OF UNIQUE XOR TRIPLETS I
+
+/** T.C. - O(N) & S.C. - O(1) **/
+
+/* C++ */
+// https://leetcode.com/problems/number-of-unique-xor-triplets-i/solutions/8415296/c-on-solution-0-ms-runtime-beats-100-by-pxnsk/
+
+/* JAVA */
+// https://leetcode.com/problems/number-of-unique-xor-triplets-i/solutions/8415297/java-solution-by-nidhi_kamal-uylv/
+
+#include<iostream>
+#include<vector>
+
+using namespace std ;
+
+int uniqueXorTriplets(vector<int>& nums) {
+    int n = nums.size() ;
+    if(n <= 2) return n ;
+    int ans = 0 ;
+    for(int num : nums) {
+        ans |= num ;
+    }
+    return ans + 1 ;
+}
+
+int main() {
+	vector<int> nums ;
+	int n, e ;
+	
+	cout << endl ;
+	cout << "  NUMBER OF UNIQUE XOR TRIPLETS I " << endl ;
+	cout << " `````````````````````````````````" << endl ;
+	
+	cout << endl ;
+	cout << "Enter size of the array, n = " ;
+	cin >> n ;
+	
+	cout << endl ;
+	cout << "Enter elements of the array....." << endl ;
+	for(int i = 0; i < n; i++){
+		cin >> e ;
+		nums.push_back(e) ;
+	}
+	
+    e = uniqueXorTriplets(nums) ;
+	
+	cout << endl ;
+	cout << "Unique triplets = " << e << endl ;
+	
+	cout << endl ;
+	
+	system("pause") ;
+	
+	return 0 ;
+}
